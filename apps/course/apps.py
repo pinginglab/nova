a1bc4263a4a8d11ec2bd478f5a178b7f1154acee
yerikyu <19802021029@139.com>
2019-05-01 15:13:25 +0800

    Wed May  1 15:13:25 CST 2019

--- apps/course/apps.py
+++ apps/course/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class CourseConfig(AppConfig):
     name = 'apps.course'
+    verbose_name = "课程管理"
--- apps/course/views.py
+++ apps/course/views.py
@@ -1,57 +1,18 @@
 # _*_ coding:utf-8 _*_
+from django.core.paginator import PageNotAnInteger
 from django.shortcuts import render
 from django.db.models import Q
 from django.views.generic.base import View
-from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
+from pure_pagination import Paginator
 
+from apps.utils.mixin_utils import LoginRequiredMixin
 from .models import Course, CourseResource, Video
-from operation.models import UserFavorite, CourseComments, UserCourse
-from utils.mixin_utils import LoginRequiredMixin
 
 
 # Create your views here.
 
 
-class CourseListView(View):
-    def get(self, request):
-        all_courses = Course.objects.all().order_by('-add_time')
-
-        # 热门课程排序
-        hot_courses = Course.objects.all().order_by("-click_nums")[:3]
-
-        # 课程搜索
-        search_keywords = request.GET.get('keywords', '')
-        if search_keywords:
-            all_courses = all_courses.filter(
-                Q(name__icontains=search_keywords) |
-                Q(desc__icontains=search_keywords) |
-                Q(detail__icontains=search_keywords)
-            )
-
-        # 课程排序
-        sort = request.GET.get('sort', '')
-        if sort == 'students':
-            all_courses = all_courses.order_by('-students')
-        elif sort == 'hot':
-            all_courses = all_courses.order_by('-click_nums')
-
-        # 对课程分页
-        try:
-            page = request.GET.get('page', 1)
-        except PageNotAnInteger:
-            page = 1
-
-        p = Paginator(all_courses, 9, request=request)
-
-        courses = p.page(page)
-
-        return render(request, 'course-list.html', {
-            'all_courses': courses,
-            'sort': sort,
-            'hot_courses': hot_courses,
-
-        })
 
 
 # 课程详情页
